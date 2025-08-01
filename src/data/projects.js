4be5ab980146353e4db5911398efa3a3d9c38699
ericfluna <ericfernandezluna@gmail.com>
2025-08-01 18:47:00 +0200

    project pagination created

---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+// src/data/projects.js
+export const projects = [
+  {
+    slug: "vaulti",
+    title: "Vaulti ~ Password Manager",
+    images: [
+      "/assets/projects/vaulti.jpg",
+      "/assets/projects/vaulti2.png",
+      "/assets/projects/vaulti3.png",
+    ], // Cambiado a ruta pública
+    description:
+      "Vaulti is a password management application focused on secure, simple, and efficient password handling. Designed to enhance users' digital security, Vaulti provides a unified platform for storing, generating, and managing strong passwords.",
+    stack: ["Flutter", "Dart", "SQLite", "argon2", "AES-256"],
+    isDone: true,
+  },
+];
