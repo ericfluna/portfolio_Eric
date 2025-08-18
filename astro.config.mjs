012e984675e400dd4fde7faa6b6c2980e8a3de3d
ericfluna <ericfernandezluna@gmail.com>
2025-08-18 08:13:41 +0200

    alias solved

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "path";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+    plugins: [tailwindcss()],
+  },
 });
